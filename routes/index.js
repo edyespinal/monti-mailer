6d4cee5271b415b5c68ec71a95837b38fa7b3c1b
Edy Espinal <contacto@edyespinal.com>
2020-12-01 23:45:04 +0100

    Added name and phone number fields to form

--- routes/index.js
+++ routes/index.js
@@ -19,8 +19,16 @@ router.post('/', parseData, async (req, res, next) => {
 	try {
 		if (domains.includes(req.headers.referer)) {
 			const data = req.body;
-			const i = await mailer(data.from, data.sender, data.recipient, data.subject, data.message);
-			res.status(200).json(i);
+			const mail = await mailer(
+				data.from,
+				data.sender,
+				data.name,
+				data.phone,
+				data.recipient,
+				data.subject,
+				data.message
+			);
+			res.status(200).json(mail);
 		} else {
 			res.status(401).json({ error: 'Unauthorized' });
 		}
@@ -36,12 +44,15 @@ module.exports = router;
  * mailer
  *
  * This function takes a contact form and mails it.
+ * @param {String} _from same as recipient because of server security requirements
  * @param {String} _sender the email address of the sender of the contact form
+ * @param {String} _name name of the sender
+ * @param {String} _phone phone number of the sender
  * @param {String} _recipient the email of the recipient of the contact form
  * @param {String} _subject the subject of the email
  * @param {String} _mensaje the message of the email
  */
-async function mailer(_from, _sender, _recipient, _subject, _mensaje) {
+async function mailer(_from, _sender, _name = '', _phone = '', _recipient, _subject, _mensaje) {
 	const transporter = nodeMailer.createTransport({
 		host: 'smtp.dreamhost.com',
 		port: 465,
@@ -56,7 +67,7 @@ async function mailer(_from, _sender, _recipient, _subject, _mensaje) {
 		from: _from,
 		to: _recipient,
 		subject: _subject,
-		html: `Email: ${_sender} <br/> Mensaje: ${_mensaje}`,
+		html: `Nombre: ${_name} <br/> Tel.: ${_phone} <br/> Mensaje: ${_mensaje}`,
 		date: Date.now()
 	});
 	return info;
